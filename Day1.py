e703b646e4f09bb281dde81da014002396908dd1
Fnight <fariscc+@gmail.com>
2019-08-21 22:00:29 +0300

    adding comments in different ways

--- Day1.py
+++ Day1.py
@@ -1,4 +1,14 @@
+#this is my first comment
+
 print("Hello world")
 
-if 5 > 4:
-    print("Five is greater than four!")
+if 5 > 3:
+    print("Five is greater than three!") #this is inline comment
+
+"""
+
+this is 
+multi line
+comments
+
+"""
